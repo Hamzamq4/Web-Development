f93450a4fab5c331106517bceb83ccddc3c2f00d
Hamzamq4 <92908776+Hamzamq4@users.noreply.github.com>
2024-04-02 22:12:51 +0200

    Tabs added

--- client/pages/Notion.jsx
+++ client/pages/Notion.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Axios from "axios";
 import { Button } from "../src/components/ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 function Notion() {
   const [name, setName] = useState("");
@@ -14,20 +15,23 @@ function Notion() {
     Axios.post("http://localhost:8000/NotionAPIPost", {
       Name: name,
       Review: review,
-    }).catch((error) => {
-      console.log(error);
-    });
+    })
+      .then(() => {
+        // Add your success logic here (e.g., a success message or redirect)
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
 
   const fetchAndDisplayAPIData = () => {
     Axios.get("http://localhost:8000/NotionAPIGet")
       .then((response) => {
         setAPIData(response.data.results);
-        console.log("API Response", response.data.results);
         setShowAPIData(true);
       })
       .catch((error) => {
-        console.log(error);
+        console.error(error);
       });
   };
 
@@ -50,36 +54,52 @@ function Notion() {
               onChange={(e) => setReview(e.target.value)}
             />
 
-            <button type="submit">Submit</button>
+            <Button type="submit">Submit</Button>
           </form>
-          <Button onClick={fetchAndDisplayAPIData}>Load Reviews</Button>{" "}
+          <Button onClick={fetchAndDisplayAPIData}>Load Reviews</Button>
         </div>
 
         {showAPIData && (
           <div className="Data">
             <p className="text-lg font-bold mb-4">API DATA</p>
-            <div className="flex flex-wrap -m-2">
-              {APIData.map((data) => {
-                if (
-                  data.properties.Name.title.length > 0 &&
-                  data.properties.Review.rich_text.length > 0
-                ) {
-                  return (
-                    <div key={data.id} className="p-2 w-1/2 md:w-1/3 lg:w-1/4">
-                      <div className="bg-white shadow-md rounded-lg overflow-hidden mb-4 p-4">
-                        <p className="text-xl font-semibold">
-                          {data.properties.Name.title[0].plain_text}
-                        </p>
-                        <p className="text-gray-600">
-                          {data.properties.Review.rich_text[0].plain_text}
-                        </p>
-                      </div>
-                    </div>
-                  );
-                }
-                return null;
-              })}
-            </div>
+            <Tabs defaultValue="Hamza" className="w-full">
+              <TabsList className="bg-black text-white">
+                <TabsTrigger value="Hamza">Hamza</TabsTrigger>
+                <TabsTrigger value="Simon">Simon</TabsTrigger>
+              </TabsList>
+
+              <TabsContent value="Hamza">
+                <div className="flex flex-wrap -m-2">
+                  {APIData.map((data) => {
+                    if (
+                      data.properties.Name.title.length > 0 &&
+                      data.properties.Review.rich_text.length > 0
+                    ) {
+                      return (
+                        <div
+                          key={data.id}
+                          className="p-2 w-1/2 md:w-1/3 lg:w-1/4"
+                        >
+                          <div className="bg-white shadow-md rounded-lg overflow-hidden mb-4 p-4 flex flex-col justify-between h-52 w-52 content-evenly">
+                            <p className="text-xl font-semibold">
+                              {data.properties.Name.title[0].plain_text}
+                            </p>
+                            <p className="text-gray-600">
+                              {data.properties.Review.rich_text[0].plain_text}
+                            </p>
+                          </div>
+                        </div>
+                      );
+                    }
+                    return null;
+                  })}
+                </div>
+              </TabsContent>
+
+              <TabsContent value="Simon">
+                <p>Her kommer Simons Testimonials</p>
+              </TabsContent>
+            </Tabs>
           </div>
         )}
       </header>
